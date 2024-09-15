313aa4ec872f5311f636d7acf3dd2f5327ad4dfe
ChrisChan8551 <chrischan8551@gmail.com>
2024-09-15 18:48:19 -0500

    GroupIterator

--- Objects/groups.js
+++ Objects/groups.js
@@ -26,9 +26,31 @@ class Group {
 		let group = new Group();
 		for (let num of collection) {
 			group.add(num);
-            console.log(num)
+			// console.log(num);
 		}
 		return group;
+	}
+	[Symbol.iterator]() {
+		return new GroupIterator(this.#members);
+	}
+}
+
+class GroupIterator {
+	#members;
+	#position;
+
+	constructor(members) {
+		this.#members = members;
+		this.#position = 0;
+	}
+	next() {
+		if (this.#position >= this.#members.length) {
+			return { done: true };
+		} else {
+			let result = { value: this.#members[this.#position], done: false };
+			this.#position += 1;
+			return result;
+		}
 	}
 }
 
@@ -41,3 +63,23 @@ group.add(10);
 group.delete(10);
 console.log(group.has(10));
 // → false
+
+// Iterable groups
+// Make the Group class from the previous exercise iterable. Refer to the section about the iterator interface earlier in the chapter if you aren’t clear on the exact form of the interface anymore.
+
+// If you used an array to represent the group’s members, don’t just return the iterator created by calling the Symbol.iterator method on the array. That would work, but it defeats the purpose of this exercise.
+
+// It is okay if your iterator behaves strangely when the group is modified during iteration.
+
+// It is probably worthwhile to define a new class GroupIterator. Iterator instances should have a property that tracks the current position in the group. Every time next is called, it checks whether it is done and, if not, moves past the current value and returns it.
+
+// The Group class itself gets a method named by Symbol.iterator that, when called, returns a new instance of the iterator class for that group.
+
+// Your code here (and the code from the previous exercise)
+
+for (let value of Group.from(['a', 'b', 'c'])) {
+	console.log(value);
+}
+// → a
+// → b
+// → c
