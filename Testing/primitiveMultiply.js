133f202c33f07682a939e949cfc6a0fcd86a22d7
ChrisChan8551 <chrischan8551@gmail.com>
2024-09-29 17:09:12 -0500

    primitiveMultiply

--- Testing/primitiveMultiply.js
+++ Testing/primitiveMultiply.js
@@ -1,14 +1,11 @@
-// Say you have a function primitiveMultiply that in 20 percent of cases multiplies two numbers and in the other 80 percent of cases raises an exception of type MultiplicatorUnitFailure. Write a function that wraps this clunky function and just keeps trying until a call succeeds, after which it returns the result.
-
+// Say you have a function primitiveMultiply that in 20 percent of cases multiplies two numbers and in the other 80 percent of cases raises an exception of type MultiplicatorUnitFailure.
+// Write a function that wraps this clunky function and just keeps trying until a call succeeds, after which it returns the result.
 // Make sure you handle only the exceptions you are trying to handle.
-
-// The call to primitiveMultiply should definitely happen in a try block. The corresponding catch block should rethrow the exception when it is not an instance of MultiplicatorUnitFailure and ensure the call is retried when it is.
-
-// To do the retrying, you can either use a loop that stops only when a call succeeds—as in the look example earlier in this chapter—or use recursion and hope you don’t get a string of failures so long that it overflows the stack (which is a pretty safe bet).
 
 class MultiplicatorUnitFailure extends Error {}
 
 function primitiveMultiply(a, b) {
+	// The call to primitiveMultiply should definitely happen in a try block. The corresponding catch block should rethrow the exception when it is not an instance of MultiplicatorUnitFailure and ensure the call is retried when it is.
 	if (Math.random() < 0.2) {
 		return a * b;
 	} else {
@@ -18,6 +15,7 @@ function primitiveMultiply(a, b) {
 
 function reliableMultiply(a, b) {
 	// Your code here.
+	// To do the retrying, you can either use a loop that stops only when a call succeeds—as in the look example earlier in this chapter—or use recursion and hope you don’t get a string of failures so long that it overflows the stack (which is a pretty safe bet).
 	// The call to primitiveMultiply should definitely happen in a try block. The corresponding catch block should rethrow the exception when it is not an instance of MultiplicatorUnitFailure and ensure the call is retried when it is.
 	for (;;) {
 		try {
